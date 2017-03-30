798bab91814a8a2e73d8b9b8751c1fa1242bf3f1
sprasad90 <prasad90@gmail.com>
2017-03-29 23:18:15 -0700

    Example 1

---
+++ ObjectExample1.java
@@ -0,0 +1,11 @@
+package misc;
+import edu.uweo.javaintro.tools.Turtle; 
+public class ObjectExample1 
+{     public static void main(String[] args)    
+{         Turtle  todd    = new Turtle();   
+      todd.move( 180, 128 );  
+       todd.move( 180, 0 );      
+   todd.paint( 90,  64 );      
+   todd.paint( 90,  64 );       
+  todd.paint( 90,  64 );        
+ todd.paint( 90,  64 );     } }
